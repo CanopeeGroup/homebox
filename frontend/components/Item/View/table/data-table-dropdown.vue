e43e326200632cf97ad6bd413b5a4bb3a7912627
leroyconstant <146106162+leroyconstant@users.noreply.github.com>
2026-09-11 16:12:21 +0200

    refactor: simplify objects and remove unused features

--- frontend/components/Item/View/table/data-table-dropdown.vue
+++ frontend/components/Item/View/table/data-table-dropdown.vue
@@ -196,26 +196,6 @@
       >
         {{ t("components.item.view.table.dropdown.change_location") }}
       </DropdownMenuItem>
-      <!-- change tags -->
-      <DropdownMenuItem
-        @click="
-          openDialog(DialogID.ItemChangeDetails, {
-            params: {
-              items: multi ? multi.items.map(row => row.original) : [item!],
-              addTags: true,
-              removeTags: true,
-            },
-            onClose: result => {
-              if (result) {
-                toast.success(t('components.item.view.table.dropdown.change_tags_success'));
-                resetSelection();
-              }
-            },
-          })
-        "
-      >
-        {{ t("components.item.view.table.dropdown.change_tags") }}
-      </DropdownMenuItem>
       <!-- maintenance -->
       <DropdownMenuItem
         @click="
